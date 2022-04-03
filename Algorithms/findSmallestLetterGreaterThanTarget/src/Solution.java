04e5e10eca6de2893c3967d41eb0a4c5b6af18d2
i1oy <luyuhang1991@gmail.com>
2022-04-03 22:03:31 +0800

    [Algo] 744. Find Smallest Letter Greater Than Target #Easy

--- Algorithms/findSmallestLetterGreaterThanTarget/src/Solution.java
+++ Algorithms/findSmallestLetterGreaterThanTarget/src/Solution.java
@@ -2,9 +2,11 @@ public class Solution {
     public static void main(String[] args) {
         Solution app = new Solution();
         System.out.println(app.nextGreatestLetter(new char[]{'a', 'b'}, 'z'));
-        System.out.println(app.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'a'));
+        System.out.println(app.nextG(new char[]{'c', 'f', 'j'}, 'a'));
         System.out.println(app.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'c'));
         System.out.println(app.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'd'));
+        System.out.println(app.nextG(new char[]{'c', 'f', 'j'}, 'z'));
+        System.out.println(app.nextG(new char[]{'e','e','e','e','e','e','n','n','n','n'}, 'z'));
     }
 
     /**
@@ -20,4 +22,13 @@ public class Solution {
         return letters[0];
     }
 
+    private char nextGreatestLetter2(char[] letters, char target) {
+        int len = letters.length, left = 0, right = len - 1;
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (letters[mid] > target) right = mid;
+            else left = mid + 1;
+        }
+        return target < letters[right] ? letters[right] : letters[0];
+    }
 }
